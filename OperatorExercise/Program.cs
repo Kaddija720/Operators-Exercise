a93f3b9116a3974baa9a451b2b86e2f9898ab23f
Kaddijatou Hydara <kad67414@gmail.com>
2025-01-18 21:12:47 -0500

    add work

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,41 @@
 {
     public class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow (radius, 2);
+        }
+
+        public static void printAreaOfCircle()
+        {
+            Console.WriteLine("what is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+                Console.WriteLine($"The area of circle is {Math.Round(AreaOfCircle(radius), 3)}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a} /{b} is {quotient} remainder {remainder}");
+            
+            printAreaOfCircle();
+            
+            int i = 3;
+            int j = 4;
+            int k = ++i * j++;
+
+            Console.WriteLine(k); // What will this output? 
+            16
+
+            
         }
     }
 }
